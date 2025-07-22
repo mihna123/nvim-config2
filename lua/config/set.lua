6f88b69fb432a62a7694a8b2e6ef67224ab6f27b
mihna123 <mihailonvojinovic@gmail.com>
2025-07-22 11:46:52 +0200

    Change home env for linux

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -18,7 +18,9 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("UserProfile") .. "/.vim/undodir"
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+-- For windows uncomment below
+-- vim.opt.undodir = os.getenv("UserProfile") .. "/.vim/undodir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
