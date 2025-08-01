aad98b16f2f909018c6b54e65c5b8f6872f8ba03
mihailo.vojinovic <mihailonvojinovic@gmail.com>
2025-08-01 09:40:10 +0200

    Add python to ensure installed for tree-sitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,11 @@
 return {
-    "nvim-treesitter/nvim-treesitter", 
-    branch = 'master', 
-    lazy = false, 
+    "nvim-treesitter/nvim-treesitter",
+    branch = 'master',
+    lazy = false,
     build = ":TSUpdate",
     opts = {
         highlight = { enable = true },
         indent = { enable = true },
-        ensure_installed = { "javascript", "lua", "typescript", "html", "css"}
+        ensure_installed = { "javascript", "lua", "typescript", "html", "css", "python"}
     }
 }
