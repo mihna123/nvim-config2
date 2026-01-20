3980ff4ed659fc7e0fd4858d6d875fbf4229655e
mihailo <mihailo@mihna123.mihna.rs>
2026-01-20 21:27:49 +0100

    Disable ts_ls by default

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -9,7 +9,7 @@ vim.lsp.enable("luau_lsp")
 vim.lsp.enable("tailwindcss")
 vim.lsp.enable("ltex")
 vim.lsp.enable("vue_ls")
-vim.lsp.enable("vtsls")
+-- vim.lsp.enable("vtsls")
 -- vim.lsp.enable("angularls"); -- When you need angular again uncomment
 
 vim.api.nvim_create_autocmd('LspAttach', {
